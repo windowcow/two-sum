4579c7e54c2c798bdef6283395dd54b5392c2b0d
windowcow <65646883+windowcow@users.noreply.github.com>
2023-03-09 19:29:34 +0900

    first answer before execution

---
+++ main.py
@@ -0,0 +1,18 @@
+def twoSum(self, nums: List[int], target: int) -> List[int]:
+  # YOUR ANSWER
+  def twoSum(nums, target):
+  hash_table = {}
+  
+  for index, num in enumerate(nums):
+    finding = target - num
+    if finding not in hash_table:
+      hash_table[num] = index
+    else:
+      return [hash_table[finding], index]
+
+
+
+# print(twoSum([2, 7, 11, 15], 9))
+# print(twoSum([3, 2, 4], 6))
+# print(twoSum([3, 3], 6)) 
+ 
